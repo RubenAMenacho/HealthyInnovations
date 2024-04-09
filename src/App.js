d4424dd68f737cae9df90576de382b60f459def7
Ruben Menacho <123403201+RubenAMenacho@users.noreply.github.com>
2024-04-09 09:42:54 -0400

    Add files via upload
    React CHAT GPT CLONE

--- src/App.js
+++ src/App.js
@@ -1,30 +1,114 @@
-// App.js
+import { useState, useEffect} from 'react'
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
-import LandingPage from './LandingPage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          {/* Removed EntryForm from here to avoid it displaying on every route */}
-          <Routes>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/landing" element={<LandingPage />} />
-            <Route path="/" element={<LandingPage />} />
-            {/* Add Route for EntryForm if needed */}
-          </Routes>
-            <br />
-        </div>
-      </Router>
-    );
+const App = () =>  {
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle]= useState(null)
+
+  const createNewChat= () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+
   }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setValue("")
+  }
+
+ const getMessages= async() => {
+   const options = {
+    method: "POST",
+    body: JSON.stringify({
+      message: value
+    }), 
+    headers: {
+      "Content-Type": "application/json"
+    }
+  
+  }
+  try{
+const response = await fetch('http://localhost:8000/completions', options)
+const data = await response.json()
+setMessage(data.choices[0].message)
+    }catch(error) {
+      console.error(error)
+    }
+
+  }
+
+useEffect(() => {
+console.log(currentTitle, value, message)
+if (!currentTitle && value && message) {
+  setCurrentTitle(value)
+}
+
+if (currentTitle && value && message){
+setPreviousChats(prevChats => ( 
+ [ ...prevChats,
+  {
+    title:currentTitle, 
+    role: "user",
+    content: value
+  },
+{
+title: currentTitle, 
+role: message.role,
+content: message.content
+}
+
+ ]
+
+))
+
+}
+}, [message, currentTitle])
+
+console.log(previousChats)
+
+const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
+console.log(uniqueTitles)
+
+  return (
+    <div className="app">
+      <section className="side-bar">
+      <button onClick= {createNewChat}>+ New chat</button>
+      <ul className="history">
+        {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick ={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+      </ul>
+      <nav>
+        <p>Made by Ruben</p>
+      </nav>
+      </section>
+      <section className="main">
+      {!currentTitle && <h1>HealthyInnovationsGPT</h1>}
+      <ul className="feed"> 
+      {currentChat?.map((chatMessage, index) => <li key = {index}>
+        <p className="role">{chatMessage.role}</p>
+        <p>{chatMessage.content}</p>
+        </li>)}
+      </ul>
+      <div className="bottom-section">
+        <div className="input-container">
+          <input value ={value} onChange={(e) => setValue(e.target.value)}/>
+          <div id="submit" onClick={getMessages}>➢</div>
+        </div>
+        <p className="info">
+
+        HealthyInnovations can make mistakes. Consider checking important information!
+    
+
+
+        </p>
+      </div>
+      </section>
+      </div>
+  
+  )
 }
 
 export default App;
-
